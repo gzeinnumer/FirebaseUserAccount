67e69f56d8d63b0bc82a83fcc072950d42be27c0
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2019-07-08 01:18:14 +0700

    logout

--- app/src/main/java/com/gzeinnumer/firebaseuseraccount/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/firebaseuseraccount/MainActivity.java
@@ -1,9 +1,14 @@
 package com.gzeinnumer.firebaseuseraccount;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,5 +32,33 @@ public class MainActivity extends AppCompatActivity {
             finish();
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         }
+    }
+
+    //todo 33
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //todo 35
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //todo 34
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //todo 36
+        switch (item.getItemId()){
+            case R.id.logout_menu:
+                //todo 37
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+            case R.id.refresh_menu:
+                break;
+            case R.id.setting_menu:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
